85faab9032222b6981fae46ec13b1741425f1a87
willikwok <willkwok86@gmail.com>
2022-10-29 14:19:48 +0700

    Add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,12 @@
+<?php
+    use 
+?>
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Data Mahasiswa</title>
+    <title>Data Mahasiswa WILLI</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
   <body>
@@ -21,11 +24,16 @@
         </div>
       </nav>
 <br>
-<br><p class="container">Nama : Willi</p>
+<br>
+<p class="container">Nama : Willi</p>
 <p class="container">Student ID : 03081200028</p>
 <p class="container">Term : 1214</p>
 <p class="container">Total SKS : 18</p>
 <h3 class="container"> Data Mahasiswa </h3>
+
+<div class="text-center">
+  <img src='poto.jpg' class="rounded" alt=".">
+</div>
 
 <table class="table table-dark table-striped container">
     <tr>
